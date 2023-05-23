a11bcb01350efbebac6904ccbf5d1fb204eb07c9
Raghu Saxena <poiasdpoiasd@live.com>
2023-05-23 19:47:12 +0800

    Fix client

--- client.mjs
+++ client.mjs
@@ -1,5 +1,11 @@
 // try and make TLS connection to socket
 import tls from 'tls';
-tls.connect('4433', '127.0.0.1');
+const c = tls.connect('4433', '127.0.0.1');
+c.enableTrace();
+c.on('error', e => {
+    console.log(e);
+    process.exit(-1);
+})
+await new Promise(r => setTimeout(r, 5000));
 console.log('Success');
 process.exit(0);
